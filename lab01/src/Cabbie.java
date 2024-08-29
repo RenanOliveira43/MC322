5297f8520c9970db2f6b432a882537144005a8c2
Renan <renanneves430@gmail.com>
2024-08-29 11:10:18 -0300

    update

--- lab01/src/Cabbie.java
+++ lab01/src/Cabbie.java
@@ -5,7 +5,6 @@ package lab01.src;
 public class Cabbie extends Person {
     private double money;
     
-    
     @Override
     public void performRole() {
         System.out.printf("Taxista dirige até o destino\n");
