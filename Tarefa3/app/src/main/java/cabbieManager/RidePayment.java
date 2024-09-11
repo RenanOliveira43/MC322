361fb15c5695b4d8124c906adaa744a296055245
Renan <renanneves430@gmail.com>
2024-09-11 10:35:57 -0300

    mudanças no RIdePayment

--- Tarefa3/app/src/main/java/cabbieManager/RidePayment.java
+++ Tarefa3/app/src/main/java/cabbieManager/RidePayment.java
@@ -1,5 +1,4 @@
 package cabbieManager;
-import java.util.UUID;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -13,76 +12,96 @@ public class RidePayment implements Payment{
     private float amount;
     private PaymentOption paymentMethod;
 
+    public String getPaymentId() {
+        return paymentId;
+    }
 
-    public RidePayment(String rideId, LocalDateTime startTime, float rideDistance, PaymentOption paymentOption) {
+    public String getRideId() {
+        return rideId;
+    }
+
+    public LocalDateTime getRideStartTime() {
+        return rideStartTime;
+    }
+
+    public float getRideDistance() {
+        return rideDistance;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public PaymentOption getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public RidePayment(String rideId, LocalDateTime startTime, float rideDistance, String paymentOption) {
         this.rideStartTime = startTime;
         this.rideDistance = rideDistance;
-        this.paymentMethod = paymentOption;
+        this.paymentMethod = PaymentOption.valueOf(paymentOption.toUpperCase());
     }
 
     public float calculateValue(float rideDistance) {
-        float initialFare = 0.0f; // Valor inicial
-        float farePerKm = 0.0f;   // Valor por km
+        float initialFee = 0.0f;
+        float feePerKm = 0.0f; 
         LocalTime startNight = LocalTime.of(18, 0); // 18:00 horas
         LocalTime endNight = LocalTime.of(6, 0);    // 06:00 horas
 
-        // Verificando se o horário está entre 18:00 e 06:00
-        boolean isNightRide = this.rideStartTime.isAfter(startNight) || this.rideStartTime.isBefore(endNight);
+        // verificando se o horário está entre 18:00 e 06:00
+        boolean isNightRide = this.rideStartTime.toLocalTime().isAfter(startNight) || this.rideStartTime.toLocalTime().isBefore(endNight);
 
-        // Aplicar as tarifas com base na distância e se é corrida noturna
+        // aplicar as tarifas com base na distância e se é corrida noturna
         if (rideDistance <= 5) {
             if (isNightRide) {
-                initialFare = 6.00f;
-                farePerKm = 2.50f;
+                initialFee = 6.00f;
+                feePerKm = 2.50f;
             } else {
-                initialFare = 5.00f;
-                farePerKm = 2.00f;
+                initialFee = 5.00f;
+                feePerKm = 2.00f;
             }
         } else if (rideDistance <= 10) {
             if (isNightRide) {
-                initialFare = 5.00f;
-                farePerKm = 3.00f;
+                initialFee = 5.00f;
+                feePerKm = 3.00f;
             } else {
-                initialFare = 4.00f;
-                farePerKm = 2.50f;
+                initialFee = 4.00f;
+                feePerKm = 2.50f;
             }
         } else if (rideDistance <= 15) {
             if (isNightRide) {
-                initialFare = 4.50f;
-                farePerKm = 3.50f;
+                initialFee = 4.50f;
+                feePerKm = 3.50f;
             } else {
-                initialFare = 3.50f;
-                farePerKm = 3.00f;
+                initialFee = 3.50f;
+                feePerKm = 3.00f;
             }
         } else if (rideDistance <= 20) {
             if (isNightRide) {
-                initialFare = 4.00f;
-                farePerKm = 4.50f;
+                initialFee = 4.00f;
+                feePerKm = 4.50f;
             } else {
-                initialFare = 3.00f;
-                farePerKm = 4.00f;
+                initialFee = 3.00f;
+                feePerKm = 4.00f;
             }
         } else { // Para distâncias maiores que 20 km
             if (isNightRide) {
-                initialFare = 3.50f;
-                farePerKm = 4.00f;
+                initialFee = 3.50f;
+                feePerKm = 4.00f;
             } else {
-                initialFare = 2.50f;
-                farePerKm = 3.50f;
+                initialFee = 2.50f;
+                feePerKm = 3.50f;
             }
         }
 
-        // Cálculo do valor total da corrida
-        float totalValue = (initialFare + (farePerKm * rideDistance)) * paymentMethod.calculatePaymentFee(totalValue);
+        // calculo do valor total da corrida
+        float totalValue = initialFee + (feePerKm * rideDistance);
+        totalValue = totalValue * paymentMethod.calculatePaymentFee(totalValue);
         this.amount = totalValue;
         
         return totalValue;
     }
 
-    
-    /**
-     * Processa o pagamento da corrida.
-    */
     public void processPayment() {
         // IMPLEMENTAR METODO PROCESS PAYMENT
     }
